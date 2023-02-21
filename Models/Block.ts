c82f3a51403a6990f48fc13e0837e912db339a75
Ghassan Aljaafri <ghassan.aljaafri@gmail.com>
2023-02-21 12:27:00 +0200

    Create block class

---
+++ Models/Block.ts
@@ -0,0 +1,35 @@
+import * as crypto from 'crypto-js';
+
+export default class Block {
+    private static currentIndex: number = 0;
+    private index: number;
+    public data: object;
+    private timestamp: Date;
+    public prevHash: string;
+    public hash: string = '';
+
+    constructor(data: object, prevHash: string = '') {
+        this.data = data;
+        this.prevHash = prevHash;
+
+        // calculated values
+        this.index = ++Block.currentIndex;
+        this.timestamp = new Date();
+        this.hash = this.calcHash();
+    }
+
+    public calcHash() {
+        return crypto.SHA256(JSON.stringify(this)).toString();
+
+        // return crypto.SHA256(this.index + JSON.stringify(this.data) + this.timestamp + this.prevHash).toString();
+    }
+
+    public setPrevHash(prevHash: string) {
+        this.prevHash = prevHash;
+        this.hash = this.calcHash();
+        return this;
+    }
+    public getIndex(): number {
+        return this.index;
+    }
+}
